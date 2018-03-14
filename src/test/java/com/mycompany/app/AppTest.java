93c3811a27201a58285b380860c126f07d8043ce
root <root@dev179.inblrlab.avaya.com>
2018-03-14 15:24:53 +0530

    14th

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -35,9 +35,9 @@ public class AppTest
         App.main(null);
         try {
           //  assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-              assertEquals("Hello Ravi U have finally found it!" + System.getProperty("line.separator"), outContent.toString());
+              assertEquals("Hello Ravi U have finally found it !" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Hello Ravi !\"");
         }
     }
 
